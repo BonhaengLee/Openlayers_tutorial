694e971fa77b048913d7277292a932f3ff8aac2e
BonhaengLee <bon952014@ajou.ac.kr>
2022-05-26 01:42:11 +0900

    Map interactions

--- Openlayers_web_map/main.js
+++ Openlayers_web_map/main.js
@@ -14,16 +14,41 @@ function init() {
       }),
     ],
     target: "js-map",
+    keyboardEventTarget: document,
   });
 
   const popupContainerElement = document.getElementById("popup-coordinates");
   const popup = new ol.Overlay({
     element: popupContainerElement,
+    positioning: "center-left",
   });
 
   map.addOverlay(popup);
 
   map.on("click", (e) => {
-    console.log(e);
+    const clickedCoordinate = e.coordinate;
+    popup.setPosition(undefined);
+    popup.setPosition(clickedCoordinate);
+    popupContainerElement.innerHTML = clickedCoordinate;
+  });
+
+  /* DragRotate Interaction */
+  const dragRotateInteraction = new ol.interaction.DragRotate({
+    condition: ol.events.condition.doubleClick,
+  });
+
+  map.addInteraction(dragRotateInteraction);
+
+  const drawInteraction = new ol.interaction.Draw({
+    type: "Polygon",
+    freehand: true,
+  });
+
+  map.addInteraction(drawInteraction);
+
+  drawInteraction.on("drawend", (e) => {
+    const parser = new ol.format.GeoJSON();
+    const drawnFeatures = parser.writeFeatures(e.feature);
+    console.log(drawnFeatures);
   });
 }
