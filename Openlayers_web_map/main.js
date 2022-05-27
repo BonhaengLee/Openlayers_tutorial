cf873414a394a41e88e712e7945452772abb5e29
BonhaengLee <bon952014@ajou.ac.kr>
2022-05-27 09:45:23 +0900

    Tiled Raster Layers

--- Openlayers_web_map/main.js
+++ Openlayers_web_map/main.js
@@ -1,79 +1,50 @@
 window.onload = init;
 
 function init() {
-  const fullScreenControl = new ol.control.FullScreen();
-  const mousePositionControl = new ol.control.MousePosition();
-  const overviewMapControl = new ol.control.OverviewMap({
-    collapsed: false,
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-      }),
-    ],
-  });
-  const scaleLineControl = new ol.control.ScaleLine();
-  const zoomSliderControl = new ol.control.ZoomSlider();
-  const zoomToExtentControl = new ol.control.ZoomToExtent();
-
   const map = new ol.Map({
     view: new ol.View({
       center: [0, 0],
-      zoom: 3,
-      minZoom: 2,
-      maxZoom: 6,
-      rotation: 0,
+      zoom: 0,
+      extent: [
+        13957035.119643355, 3886409.4235566747, 14705484.243172048,
+        4686912.891080462,
+      ],
     }),
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM(),
+        zIndex: 1,
+        visible: true,
+        extent: [
+          13957035.119643355, 3886409.4235566747, 14705484.243172048,
+          4686912.891080462,
+        ],
+        opacity: 0.5,
       }),
     ],
     target: "js-map",
-    keyboardEventTarget: document,
-    controls: ol.control
-      .defaults()
-      .extend([
-        fullScreenControl,
-        mousePositionControl,
-        overviewMapControl,
-        scaleLineControl,
-        zoomSliderControl,
-        zoomToExtentControl,
-      ]),
   });
 
-  const popupContainerElement = document.getElementById("popup-coordinates");
-  const popup = new ol.Overlay({
-    element: popupContainerElement,
-    positioning: "center-left",
+  // Layer Group
+  const layerGroup = new ol.layer.Group({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM({
+          url: "https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+        }),
+        zIndex: 0,
+        visible: true,
+        extent: [
+          13957035.119643355, 3886409.4235566747, 14705484.243172048,
+          4686912.891080462,
+        ],
+        opacity: 0.5,
+      }),
+    ],
   });
-
-  map.addOverlay(popup);
+  map.addLayer(layerGroup);
 
   map.on("click", (e) => {
-    const clickedCoordinate = e.coordinate;
-    popup.setPosition(undefined);
-    popup.setPosition(clickedCoordinate);
-    popupContainerElement.innerHTML = clickedCoordinate;
-  });
-
-  /* DragRotate Interaction */
-  const dragRotateInteraction = new ol.interaction.DragRotate({
-    condition: ol.events.condition.doubleClick,
-  });
-
-  map.addInteraction(dragRotateInteraction);
-
-  const drawInteraction = new ol.interaction.Draw({
-    type: "Polygon",
-    freehand: true,
-  });
-
-  map.addInteraction(drawInteraction);
-
-  drawInteraction.on("drawend", (e) => {
-    const parser = new ol.format.GeoJSON();
-    const drawnFeatures = parser.writeFeatures(e.feature);
-    console.log(drawnFeatures);
+    console.log(e.coordinate);
   });
 }
